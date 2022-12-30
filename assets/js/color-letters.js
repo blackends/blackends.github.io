d084d1edfd01d203b64bc5e1c92eab5122f51550
qwerji <bentswanson@gmail.com>
2022-12-29 21:29:42 -0800

    add easter egg

--- assets/js/color-letters.js
+++ assets/js/color-letters.js
@@ -17,8 +17,63 @@ function randomColor(current='') {
     return color;
 }
 
-[...document.querySelectorAll('.letter')].forEach(l => {
+const container = document.querySelector(".svg-logo");
+const letters = [...document.querySelectorAll('.letter')];
+let interval;
+
+function checkMagic() {
+    console.log('checking for magic')
+    if (allColorsMatch()) {
+        applyMagic()
+    } else {
+        removeMagic();
+    }
+}
+
+function allColorsMatch() {
+    const color = letters[0].style.fill;
+    return letters.every(l => l.style.fill == color);
+}
+
+function applyMagic() {
+    console.log('apply magic')
+    if (!letters.some(l => l.classList.contains('meander'))) {
+        console.log('magic not applied yet')
+        let t = 0;
+        letters.forEach(l => {
+            t += 1750;
+            setTimeout(() => {
+                l.classList.add('meander');
+            }, t)
+        });
+    }
+}
+
+function removeMagic() {
+    console.log('remove magic')
+    letters.forEach(l => {
+        l.classList.remove('meander');
+    });
+}
+
+function resetInterval() {
+    console.log('reset interval')
+    clearInterval(interval);
+    interval = setInterval(() => {
+        checkMagic();
+    }, 5000);
+}
+
+letters.forEach(l => {
     l.style.fill = randomColor();
-    l.addEventListener('mouseover', e => e.target.style.fill = randomColor(e.target.style.fill));
+    l.addEventListener('mouseover', e => {
+        console.log('moused over, changing color')
+        e.target.style.fill = randomColor(e.target.style.fill)
+        removeMagic();
+        resetInterval();
+    });
 });
+
+checkMagic();
+resetInterval();
 
