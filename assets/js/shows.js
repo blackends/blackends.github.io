2dee626892e52b73bfafcb0a91b0495f6d72c5bc
qwerji <bentswanson@gmail.com>
2022-12-31 02:04:11 -0800

    fix show sorting bug

--- assets/js/shows.js
+++ assets/js/shows.js
@@ -3,11 +3,18 @@ const now = new Date();
 // get all show list elements
 const shows = [...document.querySelectorAll('.show')];
 
+// a show is in the future, not in the past, until the next day
+function getComparisonDate(show) {
+    const date = new Date(show.querySelector('.date').innerText);
+    date.setDate(date.getDate() + 1);
+    return date;
+}
+
 // create a list of past shows
-const pastShows = shows.filter(show => now > new Date(show.querySelector('.date').innerText));
+const pastShows = shows.filter(show => now > getComparisonDate(show));
 
 // create a list of future shows sorted and reverse it
-const futureShows = shows.filter(show => now <= new Date(show.querySelector('.date').innerText)).reverse();
+const futureShows = shows.filter(show => now <= getComparisonDate(show)).reverse();
 
 // move the shows to their proper locations
 const pastContainer = document.querySelector('.past-container');
@@ -15,3 +22,4 @@ pastShows.forEach(show => pastContainer.appendChild(show));
 
 const futureContainer = document.querySelector('.future-container');
 futureShows.forEach(show => futureContainer.appendChild(show));
+
